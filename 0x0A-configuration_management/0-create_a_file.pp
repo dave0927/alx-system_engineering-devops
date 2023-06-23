95833a8705c0a6752b0904dfb6a62da8a52b3952
Dawit Tadesse <dawittadesse927@gmail.com>
2023-06-23 22:37:18 +0300

    TASK 0: create a file using Puppet

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# Create a fiile
+file{ '/tmp/school':
+  ensure => file,
+  path => '/tmp/shool',
+  mode => '0744',
+  owner => 'www-data',
+  group => 'www-data',
+  content => 'I love Puppet'
+}
