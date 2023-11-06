8533913cb6b0adf7fec83eddfc901483d66584a9
Dawit Tadesse <114774535+dave0927@users.noreply.github.com>
2023-11-06 20:07:34 +0300

    Delete 0x0A-configuration_management directory

--- 0x0A-configuration_management/0-create_a_file.pp
+++
@@ -1,9 +0,0 @@
-# Create a fiile
-file { '/tmp/school':
-  ensure  => file,
-  path    => '/tmp/school',
-  mode    => '0744',
-  owner   => 'www-data',
-  group   => 'www-data',
-  content => 'I love Puppet'
-}
