057aa2b9afefa59c0e5a21e26aea88ec2dcbe775
Dawit Tadesse <dawittadesse927@gamil.com>
2023-12-26 10:56:06 +0000

    fix: semicolon

--- 0x14-mysql/0-create-user.sql
+++ 0x14-mysql/0-create-user.sql
@@ -1,4 +1,4 @@
 -- A sql script to create a user with password
 -- Grant replication previlage
-CREATE USER 'holberton_user'@'localhost' IDENTIFIED BY 'projectcorrection280hbtn'
-GRANT REPLICATION CLIENT ON *.* TO 'holberton_user'@'localhost'
+CREATE USER 'holberton_user'@'localhost' IDENTIFIED BY 'projectcorrection280hbtn';
+GRANT REPLICATION CLIENT ON *.* TO 'holberton_user'@'localhost';
